f36344cca2e565f321384c892636c3b14cfebefb
imzkj <601869492@qq.com>
2017-03-21 11:15:49 +0800

    cc

--- src/com/service/FileServiceAction.java
+++ src/com/service/FileServiceAction.java
@@ -2,8 +2,10 @@ package com.service;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.tool.DataBaseOperation;
+import com.tool.FileMd5Utils;
 import com.tool.HDFSOperation;
 
+import java.io.*;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +13,39 @@ import java.util.List;
 import com.common.File;
 import org.apache.struts2.ServletActionContext;
 
+import org.apache.struts2.ServletActionContext;
+
 /**
  * Created by ZKJ on 2017/3/16 0016.
  */
 public class FileServiceAction extends ActionSupport {
-    private HDFSOperation hdfsOperation;
-    private DataBaseOperation dataBaseOperation;
+    private HDFSOperation hdfsOperation=new HDFSOperation();
+    private DataBaseOperation dataBaseOperation=new DataBaseOperation();
+    private FileMd5Utils md5=new FileMd5Utils();
     private String localPath;
     private String dbPath;
     private String hdfsPath;
+    private java.io.File filename;
+    private String filenameFileName;
+
+    public FileServiceAction() throws Exception {
+    }
+
+    public String getFilenameFileName() {
+        return filenameFileName;
+    }
+
+    public void setFilenameFileName( String filenameFileName ) {
+        this.filenameFileName = filenameFileName;
+    }
+
+    public java.io.File getFilename() {
+        return filename;
+    }
+
+    public void setFilename( java.io.File filename ) {
+        this.filename = filename;
+    }
 
     public static List<File> fileslist = new ArrayList<File>();
 
@@ -52,13 +78,28 @@ public class FileServiceAction extends ActionSupport {
     }
 
     public String uploadFile() throws Exception {
-
-        System.out.println(dbPath + ":" + localPath + "  " + ServletActionContext.getContext().getName());
+        FileInputStream is = new FileInputStream(getFilename());
+       System.out.println(md5.getFileMD5String(filename));
+//        BufferedReader tBufferedReader = new BufferedReader(new InputStreamReader(is));
+//        StringBuffer tStringBuffer = new StringBuffer();
+//        String sTempOneLine = new String("");
+//        while((sTempOneLine = tBufferedReader.readLine()) != null) {
+//            tStringBuffer.append(sTempOneLine);
+//        }
+//        System.out.println("输入文件内容:" + tStringBuffer.toString());
+//        System.out.println("输入文件名字:" + filenameFileName);
+//        OutputStream os = new FileOutputStream(hdfsPath);
+//        byte buffer[] = new byte[1024];
+//        int cnt = 0;
+//        while((cnt = is.read(buffer)) > 0) {
+//            os.write(buffer, 0, cnt);
+//        }
+//        os.close();
+//        is.close();
         return "ok";
     }
 
     public String listAll() throws Exception {
-        dataBaseOperation = new DataBaseOperation();
         String sql = "select * from file ";
         //"where dbpath=\"" + path + "\"";
         ResultSet resultSet = dataBaseOperation.querySql(sql);
